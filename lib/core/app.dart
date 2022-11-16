b919614e6d04fdb3b537eec1ea82da51e2120116
osamasabry9 <osamasabry324@gmail.com>
2022-11-16 02:06:32 +0200

    Added my project

---
+++ lib/core/app.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:music_app/presentations/screens/home_screen.dart';
+import 'package:music_app/presentations/screens/playlist_screen.dart';
+import 'package:music_app/presentations/screens/song_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp._internal();
+  static const MyApp _instance = MyApp._internal();
+  factory MyApp() => _instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.white,
+              displayColor: Colors.white,
+            ),
+      ),
+      home: const HomeScreen(),
+      getPages: [
+        GetPage(name: '/', page: () => const HomeScreen()),
+        GetPage(name: '/song', page: () => const SongScreen()),
+        GetPage(name: '/playlist', page: () => const PlaylistScreen()),
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'core/app.dart';
+
+void main() {
+  runApp( MyApp());
+}
